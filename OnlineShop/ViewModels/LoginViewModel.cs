a74fbb77a36f1b48c8259a84bfbd18068bb2e43f
miloszkulesza <milosz.kulesza1@gmail.com>
2019-10-15 13:54:32 +0200

    strona logowania, aktualizacja bibliotek css i js

---
+++ OnlineShop/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Pole email jest wymagane")]
+        [UIHint("email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Pole hasło jest wymagane")]
+        [UIHint("password")]
+        public string Password { get; set; }
+    }
+}
