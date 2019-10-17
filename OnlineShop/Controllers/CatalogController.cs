b4e340187d9fe9e38a0d461971743d6e8a38d62c
miloszkulesza <milosz.kulesza1@gmail.com>
2019-10-17 16:05:22 +0200

    sortowanie produktów po kategoriach

--- OnlineShop/Controllers/CatalogController.cs
+++ OnlineShop/Controllers/CatalogController.cs
@@ -5,28 +5,39 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Data;
 using OnlineShop.Models;
+using OnlineShop.ViewModels;
 
 namespace OnlineShop.Controllers
 {
     public class CatalogController : Controller
     {
         private IProductRepository productRepository;
+        private ICategoryRepository categoryRepository;
 
-        public CatalogController(IProductRepository productRepository)
+        public CatalogController(IProductRepository productRepository,
+            ICategoryRepository categoryRepository)
         {
             this.productRepository = productRepository;
+            this.categoryRepository = categoryRepository;
         }
 
         public IActionResult Index(string categoryId)
         {
-            List<Product> products;
-            if(categoryId == null)
+            ProductsListViewModel productsViewModel;
+            if (categoryId == null)
             {
-                products = productRepository.Products.OrderBy(p => p.DateOfAddition).ToList();
-                return View(products);
+                productsViewModel = new ProductsListViewModel
+                {
+                    Products = productRepository.Products.OrderBy(p => p.DateOfAddition).ToList()
+                };
+                return View(productsViewModel);
             }
-            products = productRepository.Products.Where(p => p.Category.Id == categoryId).OrderBy(p => p.DateOfAddition).ToList();
-            return View(products);
+            productsViewModel = new ProductsListViewModel
+            {
+                Products = productRepository.Products.Where(p => p.Category.Id == categoryId).OrderBy(p => p.DateOfAddition).ToList(),
+                Category = categoryRepository.Categories.FirstOrDefault(c => c.Id == categoryId)
+            };
+            return View(productsViewModel);
         }
     }
 }
---
+++ OnlineShop/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public Category Category { get; set; }
+    }
+}
