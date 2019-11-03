c4234c1a7a5f7a8923b218fd5e3c6b89ced9d7c2
miloszkulesza <milosz.kulesza1@gmail.com>
2019-11-03 17:02:07 +0100

    wyszukiwarka

---
+++ OnlineShop/Controllers/SearchController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+using OnlineShop.ViewModels;
+
+namespace OnlineShop.Controllers
+{
+    public class SearchController : Controller
+    {
+        private IProductRepository productRepository;
+
+        public SearchController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Search(string id)
+        {
+            if (id == null || id.Equals(""))
+            {
+                return View();
+            }
+            SearchViewModel searchViewModel = new SearchViewModel();
+
+            searchViewModel.SearchText = id;
+            searchViewModel.Products = productRepository.Products.Where(product => product.Name.ToLower().Contains(id.ToLower()));
+
+            if (searchViewModel.Products.Any())
+                searchViewModel.AnySearchingProductExists = true;
+            else searchViewModel.AnySearchingProductExists = false;
+
+            return View(searchViewModel);
+        }
+    }
+}
---
+++ OnlineShop/ViewModels/SearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.ViewModels
+{
+    public class SearchViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public bool AnySearchingProductExists { get; set; } = false;
+        public string SearchText { get; set; }
+    }
+}
